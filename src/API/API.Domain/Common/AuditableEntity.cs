ae5cf7d603b6f33871ff32ca58bf95b6b521d246
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-11 14:55:51 +0200

    Base domain layer functional.

---
+++ src/API/API.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
