3449d851bd56b116ebe6e79f50c3eb1f3f1e63c1
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-15 09:33:08 +0200

    Fixed API project skeleton.

---
+++ src/Client/ApiService/Categories/ICategoryApiService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiService.Categories
+{
+    public class ICategoryApiService
+    {
+    }
+}
