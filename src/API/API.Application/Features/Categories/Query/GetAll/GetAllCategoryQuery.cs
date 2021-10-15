d35447854fea33d1b686ba53a0e049cdb9a0fa64
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-15 09:20:56 +0200

    Added Category Handler at API Level & records in Ocelot.

---
+++ src/API/API.Application/Features/Categories/Query/GetAll/GetAllCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace API.Application.Features.Categories.Query.GetAll
+{
+    public class GetAllCategoryQuery : IRequest<List<CategoryVm>>
+    {
+    }
+}
