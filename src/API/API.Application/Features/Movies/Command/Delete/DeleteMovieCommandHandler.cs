e53d6b2ad24c3a6b18b5c8cbd133f1b788237084
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-18 09:28:03 +0200

    Delete Movie implemented on API side.

---
+++ src/API/API.Application/Features/Movies/Command/Delete/DeleteMovieCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using API.Application.Contracts.Identity;
+using API.Application.Contracts.Persistence;
+using API.Application.Exceptions;
+using API.Domain.Entities;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Features.Movies.Command.Delete
+{
+    public class DeleteMovieCommandHandler : IRequestHandler<DeleteMovieCommand>
+    {
+        private readonly IMovieRepository _movieRepository;
+        private readonly ILoggedInUserService _loggedInUserService;
+
+        public DeleteMovieCommandHandler(IMovieRepository movieRepository, ILoggedInUserService loggedInUserService)
+        {
+            _movieRepository = movieRepository ?? throw new ArgumentNullException(nameof(movieRepository));
+            _loggedInUserService = loggedInUserService ?? throw new ArgumentNullException(nameof(loggedInUserService));
+        }
+        public async Task<Unit> Handle(DeleteMovieCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _movieRepository.GetByIdAndOwnerId(request.Id, _loggedInUserService.UserId);
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Movie), request.Id);
+            }
+
+            await _movieRepository.DeleteAsync(entity);
+            return Unit.Value;
+
+        }
+    }
+}
--- src/API/MovieAPI/Controllers/MovieController.cs
+++ src/API/MovieAPI/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Application.Contracts.Identity;
 using API.Application.Features.Movies.Command.Create;
+using API.Application.Features.Movies.Command.Delete;
 using API.Application.Features.Movies.Query.Get;
 using API.Application.Features.Movies.Query.GetAll;
 using Microsoft.AspNetCore.Authorization;
@@ -12,18 +13,10 @@ namespace MovieAPI.Controllers
 {
     public class MovieController : ApiController
     {
-        private readonly ILoggedInUserService _loggedInUserService;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public MovieController(ILoggedInUserService loggedInUserService, IHttpContextAccessor httpContextAccessor)
-        {
-            _loggedInUserService = loggedInUserService ?? throw new ArgumentNullException(nameof(loggedInUserService));
-            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
-        }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(Guid id)
         {
-            var sub = _loggedInUserService.UserId;
             return Ok(await Mediator.Send(new GetMovieQuery(id)));
         }
 
@@ -38,6 +31,12 @@ namespace MovieAPI.Controllers
         public async Task<IActionResult> Post([FromBody] CreateMovieCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(new DeleteMovieCommand(id));
         }
     }
 }
