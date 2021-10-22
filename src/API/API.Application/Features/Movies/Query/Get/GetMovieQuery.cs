0d643f132a59ef8dce163d4affbefc90b02f885a
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-22 10:53:57 +0200

    Get All and Get movie working.

--- src/API/API.Application/Features/Movies/Query/Get/GetMovieQuery.cs
+++ src/API/API.Application/Features/Movies/Query/Get/GetMovieQuery.cs
@@ -1,9 +1,10 @@
-﻿using MediatR;
+﻿using API.Application.Response;
+using MediatR;
 using System;
 
 namespace API.Application.Features.Movies.Query.Get
 {
-    public class GetMovieQuery : IRequest<MovieVm>
+    public class GetMovieQuery : IRequest<ApiResponse<MovieVm>>
     {
         public GetMovieQuery(Guid id)
         {
--- src/Client/ApiService/ApiResponseParserService.cs
+++ src/Client/ApiService/ApiResponseParserService.cs
@@ -74,13 +74,13 @@ namespace Movies.Client.ApiService
             {
                 return response;
             }
+            var deserialized = await JsonSerializer.DeserializeAsync<BaseResponse<T>>(stream, _options);
+
             if (!listExpected)
             {
-                var data = await JsonSerializer.DeserializeAsync<T>(stream, _options);
-                response.Data = data;
+                response.Data = deserialized.Data;
                 return response;
             }
-            var deserialized = await JsonSerializer.DeserializeAsync<BaseResponse<T>>(stream, _options);
             response.DataList = deserialized.DataList;
             return response;
         }
