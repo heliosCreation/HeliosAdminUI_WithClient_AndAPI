866587c47157ae92eaa7f11bc62424ac537a7adf
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-22 14:45:37 +0200

    Moved swagger service to its own service extensions on API.

---
+++ src/API/MovieAPI/Extensions/SwaggerServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace MovieAPI.Extensions
+{
+    public static class SwaggerServiceExtension
+    {
+        public static IServiceCollection AddSwaggerService(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Movie - WebApi",
+                        Description = "This Api will be responsible for managing the movies and completing claim bases.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "HeliosCreation",
+                            Email = "reliableDevelopment@hotmail.com",
+                        }
+                    });
+            });
+            return services;
+        }
+    }
+}
