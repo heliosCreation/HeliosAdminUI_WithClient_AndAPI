91492a7edec245250a928d65252cf664667e029e
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-25 14:06:15 +0200

    Removed Comments in APIResponse.

--- src/API/API.Application/Response/ApiResponse.cs
+++ src/API/API.Application/Response/ApiResponse.cs
@@ -5,30 +5,6 @@ namespace API.Application.Response
 {
     public class ApiResponse<T> : BaseResponse where T:class 
     {
-        //public ApiResponse()
-        //{
-        //    Succeeded = true;
-        //}
-
-        //public ApiResponse(string message = null)
-        //{
-        //    Succeeded = true;
-        //    Message = message;
-        //}
-
-        //public ApiResponse(string message, bool success)
-        //{
-        //    Succeeded = success;
-        //    Message = message;
-        //}
-
-        //public bool Succeeded { get; set; } = true;
-
-        //public string Message { get; set; }
-
-        //public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
-
-        //public List<string> ErrorMessages { get; set; }
 
         public T Data { get; set; }
 
