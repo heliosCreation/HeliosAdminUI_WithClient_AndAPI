539d51e6a57710c354eb293ff8f2c711247e85d4
Quentin Couissinier <reliabledevelopment@outlook.com>
2021-10-25 14:04:33 +0200

    Fixed Behaviour pipeline causing wrong response format in Swagger.

--- src/API/API.Application/Response/ApiResponse.cs
+++ src/API/API.Application/Response/ApiResponse.cs
@@ -3,32 +3,32 @@ using System.Net;
 
 namespace API.Application.Response
 {
-    public class ApiResponse<T> where T:class
+    public class ApiResponse<T> : BaseResponse where T:class 
     {
-        public ApiResponse()
-        {
-            Succeeded = true;
-        }
+        //public ApiResponse()
+        //{
+        //    Succeeded = true;
+        //}
 
-        public ApiResponse(string message = null)
-        {
-            Succeeded = true;
-            Message = message;
-        }
+        //public ApiResponse(string message = null)
+        //{
+        //    Succeeded = true;
+        //    Message = message;
+        //}
 
-        public ApiResponse(string message, bool success)
-        {
-            Succeeded = success;
-            Message = message;
-        }
+        //public ApiResponse(string message, bool success)
+        //{
+        //    Succeeded = success;
+        //    Message = message;
+        //}
 
-        public bool Succeeded { get; set; } = true;
+        //public bool Succeeded { get; set; } = true;
 
-        public string Message { get; set; }
+        //public string Message { get; set; }
 
-        public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
+        //public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
 
-        public List<string> ErrorMessages { get; set; }
+        //public List<string> ErrorMessages { get; set; }
 
         public T Data { get; set; }
 
